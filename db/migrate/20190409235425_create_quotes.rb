37191c26ee7e80dffb8e05d28a62ddfd424719cd
Samantha Lee <samanthal.lee3@gmail.com>
2019-04-09 23:56:16 +0000

    final updated migrate

--- db/migrate/20190409235425_create_quotes.rb
+++ db/migrate/20190409235425_create_quotes.rb
@@ -1,7 +1,8 @@
 class CreateQuotes < ActiveRecord::Migration[5.2]
   def change
     create_table :quotes do |t|
-
+      t.string :saying
+      t.string :author
       t.timestamps
     end
   end
